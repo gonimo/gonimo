1f514324ac716e4133ad8ffd7252f6d7b6b0ad34
Robert Klotzner <robert.klotzner@gmx.at>
2016-06-24 09:39:11 +0200

    Dropped DbTypes

--- src/Gonimo/Types.hs
+++ src/Gonimo/Types.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE InstanceSigs        #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
 
 module Gonimo.Types where
 
@@ -16,6 +20,18 @@ import           Web.HttpApiData       (FromHttpApiData (..),
                                         parseUrlPieceWithPrefix)
 
 import           Data.Text             (Text)
+import           Control.Error.Safe (rightZ)
+import           Data.Aeson.Types       (FromJSON (..), ToJSON (..), Value (String), defaultOptions, genericToJSON)
+import           Data.ByteString (ByteString)
+
+import qualified Data.ByteString.Base64 as Base64
+import           Data.Bifunctor
+import           Data.Text as T
+import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import           Database.Persist.TH
+import           GHC.Generics (Generic)
+{-import           Servant.Common.Text (FromText (..), ToText (..))-}
+import           Web.HttpApiData (FromHttpApiData(..))
 
 type SenderName = Text
 
@@ -79,3 +95,33 @@ instance ToJSON Coffee where
 
 type FamilyName = Text
 
+type EmailAddress = Text
+
+
+--------------------------------------------------
+data InvitationDelivery = EmailInvitation EmailAddress
+                        | OtherDelivery
+                        deriving (Read, Show, Generic)
+
+instance FromJSON InvitationDelivery
+
+instance ToJSON InvitationDelivery where
+  toJSON = genericToJSON defaultOptions
+  -- toEncoding = genericToEncoding defaultOptions
+
+derivePersistField "InvitationDelivery"
+--------------------------------------------------
+
+newtype Secret = Secret ByteString deriving (Generic, Show, Read)
+
+instance FromJSON Secret where
+  parseJSON (String t) = Secret <$> (rightZ . Base64.decode . encodeUtf8 $ t)
+  parseJSON _ = fail "Expecting a string when parsing a secret."
+
+instance ToJSON Secret where
+  toJSON (Secret bs) = String . decodeUtf8 . Base64.encode $ bs
+u
+instance FromHttpApiData Secret where
+    parseUrlPiece = bimap T.pack Secret . Base64.decode . encodeUtf8
+
+derivePersistField "Secret"
