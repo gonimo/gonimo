0557cda4fd17132a71d9d597110113bfde536b97
Robert Klotzner <robert.klotzner@gmx.at>
2016-10-20 13:43:34 +0200

    Bumped version

--- stack.nix
+++ stack.nix
@@ -6,7 +6,7 @@ in
   haskell.lib.buildStackProject {
   name = "myEnv";
   #    buildInputs = [ gcc git zlib pkgconfig ghc glibcLocales ];
-  buildInputs = [ zlib haskellPackages.ghc-mod postgresql openssl ];
+  buildInputs = [ zlib haskellPackages.ghc-mod haskellPackages.cabal-install haskellPackages.hlint postgresql openssl ];
     ghc = ghc;
     shellHook = "export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt";
 }
