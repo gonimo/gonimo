e119d619bb42867db7777e7268cb5ecd415eb685
Robert Klotzner <robert.klotzner@gmx.at>
2016-11-14 11:01:37 +0100

    Remove socket handlers from Auth.hs

--- src/Gonimo/Server/Handlers/Auth.hs
+++ src/Gonimo/Server/Handlers/Auth.hs
@@ -3,52 +3,55 @@
 {-# LANGUAGE RankNTypes #-}
 module Gonimo.Server.Handlers.Auth where
 
-import           Control.Concurrent.STM              (STM, TVar, readTVar)
+import           Control.Applicative                  ((<|>))
+import           Control.Concurrent.STM               (STM, TVar, readTVar)
 import           Control.Lens
 import           Control.Monad
-import           Control.Monad.Extra                 (whenM)
-import           Control.Monad.Freer                 (Eff)
-import           Control.Monad.Freer.Reader          (ask)
-import           Control.Monad.Trans.Maybe      (MaybeT (MaybeT), runMaybeT)
-import           Control.Monad.State.Class           (gets, modify)
-import           Control.Monad.Trans.State      (StateT (..))
-import           Control.Monad.STM.Class             (liftSTM)
-import           Control.Monad.Trans.Identity   (runIdentityT, IdentityT)
-import           Control.Monad.Trans.Class           (lift)
-import           Control.Monad.Trans.Maybe           (MaybeT (..), runMaybeT)
-import qualified Data.Map.Strict                     as M
+import           Control.Monad.Extra                  (whenM)
+import           Control.Monad.Freer                  (Eff)
+import           Control.Monad.Freer.Reader           (ask)
+import           Control.Monad.Freer.Exception        (throwError)
+import           Control.Monad.State.Class            (gets, modify)
+import           Control.Monad.STM.Class              (liftSTM)
+import           Control.Monad.Trans.Class            (lift)
+import           Control.Monad.Trans.Identity         (IdentityT, runIdentityT)
+import           Control.Monad.Trans.Maybe            (MaybeT (MaybeT),
+                                                       runMaybeT)
+import           Control.Monad.Trans.Maybe            (MaybeT (..), runMaybeT)
+import           Control.Monad.Trans.State            (StateT (..))
+import qualified Data.Map.Strict                      as M
+import           Data.Maybe                           (fromMaybe)
 import           Data.Monoid
-import           Data.Maybe                          (fromMaybe)
-import           Data.Proxy                          (Proxy (..))
-import qualified Data.Set                            as S
-import           Data.Text                           (Text)
-import qualified Data.Text                           as T
-import           Database.Persist                    (Entity (..), (==.))
-import qualified Gonimo.Database.Effects             as Db
-import qualified Gonimo.Database.Effects             as Db
-import           Gonimo.Database.Effects.Servant     as Db
-import           Gonimo.Server.Auth                  as Auth
-import           Gonimo.Server.Handlers.Auth.Internal
+import           Data.Proxy                           (Proxy (..))
+import qualified Data.Set                             as S
+import           Data.Text                            (Text)
+import qualified Data.Text                            as T
+import           Database.Persist                     (Entity (..), (==.))
+import           Database.Persist.Class
+import qualified Gonimo.Database.Effects              as Db
+import qualified Gonimo.Database.Effects              as Db
+import           Gonimo.Database.Effects.Servant      as Db
+import           Gonimo.Server.Auth                   as Auth
 import           Gonimo.Server.Db.Entities
 import           Gonimo.Server.Effects
 import           Gonimo.Server.EmailInvitation
 import           Gonimo.Server.Error
+import           Gonimo.Server.Handlers.Auth.Internal
 import           Gonimo.Server.State
+import qualified Gonimo.Server.State.MessageBox       as MsgBox
 import           Gonimo.Server.State.Types
 import           Gonimo.Server.Types
-import           Gonimo.WebAPI                       (ReceiveChannelR,
-                                                      ReceiveChannelR)
-import           Gonimo.WebAPI.Types                 (InvitationInfo (..),
-                                                      InvitationReply (..))
-import qualified Gonimo.WebAPI.Types                 as Client
-import           Servant.API                         ((:>))
-import           Servant.Server                      (ServantErr (..), err400,
-                                                      err403)
-import           Servant.Subscriber                  (Event (ModifyEvent))
-import           Utils.Control.Monad.Trans.Maybe     (maybeT)
-import Database.Persist.Class
-import Unsafe.Coerce -- For debugging - really!
-import           Gonimo.Server.State.MessageBox      as MsgBox
+import           Gonimo.WebAPI                        (ReceiveChannelR,
+                                                       ReceiveMessageR)
+import           Gonimo.WebAPI.Types                  (InvitationInfo (..),
+                                                       InvitationReply (..))
+import qualified Gonimo.WebAPI.Types                  as Client
+import           Servant.API                          ((:>))
+import           Servant.Server                       (ServantErr (..), err400,
+                                                       err403)
+import           Servant.Subscriber                   (Event (ModifyEvent))
+import           Unsafe.Coerce
+import           Utils.Control.Monad.Trans.Maybe      (maybeT)
 
 createInvitation :: AuthServerConstraint r => FamilyId -> Eff r (InvitationId, Invitation)
 createInvitation fid = do
@@ -181,75 +184,6 @@ getFamily familyId = do
   authorizeAuthData $ isFamilyMember familyId
   runDb $ Db.getErr (NoSuchFamily familyId) familyId
 
--- | Create a channel for communication with  a baby station
---
---   The baby station must call receiveChannel within a given timeout,
---   this handler will only return a secret if the baby station did so,
---   otherwise an error is thrown (not found - `NoSuchSocket`)
-createChannel :: AuthServerConstraint r
-              => FamilyId -> DeviceId -> DeviceId -> Eff r Secret
-createChannel familyId toId fromId = do
-  authorizeAuthData (isFamilyMember familyId)
-  authorizeAuthData ((fromId ==) . deviceKey)
-
-  secret <- generateSecret
-  updateFamilyRetryEff SocketBusy familyId $ MsgBox.setData channelSecrets toId (fromId, secret)
-
-  notify ModifyEvent endpoint (\f -> f familyId toId)
-
-  waitForReaderEff NoSuchSocket familyId toId channelSecrets
-  return secret
- where
-
-   endpoint :: Proxy ("socket" :> ReceiveChannelR)
-   endpoint = Proxy
-
-
-receiveChannel :: AuthServerConstraint r
-               => FamilyId -> DeviceId -> Eff r (Maybe (MessageNumber, ChannelRequest))
--- in this request @to@ is the one receiving the secret
-receiveChannel familyId toId = do
-    authorizeAuthData (isFamilyMember familyId)
-    authorizeAuthData ((toId ==) . deviceKey)
-
-    MsgBox.getData toId . _channelSecrets <$> getFamilyEff familyId
-
-deleteChannelRequest :: AuthServerConstraint r
-                        =>  FamilyId -> DeviceId ->  TODO! :-)
-
-putChannel :: forall r. AuthServerConstraint r
-           => FamilyId -> DeviceId -> DeviceId -> Secret -> Text -> Eff r ()
-putChannel familyId fromId toId secret txt = do
-    authorizeAuthData (isFamilyMember familyId)
-    authorizeAuthData ((fromId ==) . deviceKey)
-
-    let key = (fromId, toId, secret)
-    updateFamilyRetryEff ChannelBusy familyId $ MsgBox.setData channelData key txt
-
-    notify ModifyEvent endpoint (\f -> f familyId fromId toId secret)
-
-    waitForReaderEff NoSuchChannel familyId key channelData
-  where
-
-   endpoint :: Proxy ("socket" :> ReceiveChannelR)
-   endpoint = Proxy
-
-
-receiveChannel :: AuthServerConstraint r
-               => FamilyId -> DeviceId -> DeviceId -> Secret -> Eff r (Maybe Text)
-receiveChannel familyId fromId toId secret = do
-  authorizeAuthData (isFamilyMember familyId)
-  authorizeAuthData ((toId ==) . deviceKey)
-
-  MsgBox.getData (fromId, toId, secret) . _channelData <$> getFamilyEff familyId
-
-markReadChannel :: AuthServerConstraint r => FamilyId -> DeviceId -> DeviceId -> Secret -> Eff r ()
-markReadChannel familyId fromId toId secret = do
-    let key = (fromId, toId, secret)
-    updateFamilyErrEff familyId $ markRead key channelData
-
-
-markReadChanError :: UpdateFamilyT (ExceptT )
 -- Internal function for debugging:
 prettyKey :: Key a -> Text
 prettyKey = T.pack . show . (unsafeCoerce :: Key a -> Int)
