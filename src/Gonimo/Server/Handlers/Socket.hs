0b4da9df3259d3275d642eac3da2d81bfc776e71
Robert Klotzner <robert.klotzner@gmx.at>
2016-11-16 08:06:07 +0100

    Added debugging output

--- src/Gonimo/Server/Handlers/Socket.hs
+++ src/Gonimo/Server/Handlers/Socket.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE Unsafe #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Gonimo.Server.Handlers.Socket where
 
+import           Data.Monoid
 import           Control.Applicative                  ((<|>))
 import           Control.Monad.Freer                  (Eff)
 import           Control.Monad.Trans.Maybe            (runMaybeT)
@@ -18,6 +20,9 @@ import           Servant.API                          ((:>))
 import           Servant.Subscriber                   (Event (ModifyEvent))
 import           Control.Monad.Trans.Class            (lift)
 import           Gonimo.Server.Db.Entities            (FamilyId, DeviceId)
+import           Unsafe.Coerce
+import qualified Data.Text                            as T
+import           Database.Persist.Class
 
 -- | Create a channel for communication with  a baby station
 --
@@ -31,11 +36,15 @@ createChannel familyId toId fromId = do
   authorizeAuthData ((fromId ==) . deviceKey)
 
   secret <- generateSecret
+  $(logDebug) $ "SOCKET: create (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): waiting for socket to become clear ...."
   updateFamilyRetryEff SocketBusy familyId $ MsgBox.setData channelSecrets toId (fromId, secret)
 
+  $(logDebug) $ "SOCKET: create (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): notifying reader ...."
   notify ModifyEvent endpoint (\f -> f familyId toId)
 
+  $(logDebug) $ "SOCKET: create (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): waiting for reader...."
   waitForReaderEff NoSuchSocket familyId toId channelSecrets
+  $(logDebug) $ "SOCKET: create (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): quitting ...."
   return secret
  where
    endpoint :: Proxy ("socket" :> ReceiveChannelR)
@@ -49,6 +58,7 @@ receiveChannel familyId toId = do
     authorizeAuthData (isFamilyMember familyId)
     authorizeAuthData ((toId ==) . deviceKey)
 
+    $(logDebug) $ "SOCKET: receiveChannel (" <> prettyKey toId <>"): getting channel key ...."
     fmap fst . MsgBox.getData toId . _channelSecrets <$> getFamilyEff familyId
 
 deleteChannelRequest :: AuthServerConstraint r
@@ -57,6 +67,7 @@ deleteChannelRequest familyId toId fromId secret = do
     authorizeAuthData (isFamilyMember familyId)
     authorizeAuthData ((toId ==) . deviceKey)
 
+    $(logDebug) $ "SOCKET: delete (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): marking as read ...."
     let chanRequest = (fromId, secret)
     _ :: Maybe () <- runMaybeT $ do
       let markRead = mayUpdateFamilyEff familyId $ MsgBox.markRead channelSecrets toId chanRequest
@@ -70,11 +81,15 @@ putMessage familyId fromId toId secret txt = do
     authorizeAuthData ((fromId ==) . deviceKey)
 
     let key = (fromId, toId, secret)
+    $(logDebug) $ "SOCKET: putMessage (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): waiting for channel ...."
     updateFamilyRetryEff ChannelBusy familyId $ MsgBox.setData channelData key txt
 
+    $(logDebug) $ "SOCKET: putMessage (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): notifying reader ...."
     notify ModifyEvent endpoint (\f -> f familyId fromId toId secret)
 
+    $(logDebug) $ "SOCKET: putMessage (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): waiting for reader ...."
     waitForReaderEff NoSuchChannel familyId key channelData
+    $(logDebug) $ "SOCKET: putMessage (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): quitting ...."
   where
 
    endpoint :: Proxy ("socket" :> ReceiveMessageR)
@@ -87,6 +102,7 @@ receiveMessage familyId fromId toId secret = do
   authorizeAuthData (isFamilyMember familyId)
   authorizeAuthData ((toId ==) . deviceKey)
 
+  $(logDebug) $ "SOCKET: receiveMessage (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <>"): reading data ...."
   MsgBox.getData (fromId, toId, secret) . _channelData <$> getFamilyEff familyId
 
 deleteMessage :: AuthServerConstraint r => FamilyId -> DeviceId -> DeviceId -> Secret
@@ -95,5 +111,10 @@ deleteMessage familyId fromId toId secret num = do
     let key = (fromId, toId, secret)
     _ :: Maybe () <-runMaybeT $ do
       let markRead = mayUpdateFamilyEff familyId $ MsgBox.markRead channelData key num
+      lift $ $(logDebug) $ "SOCKET: deleteMessage (" <> prettyKey toId <> ", " <> prettyKey fromId <> ", " <> (T.pack . show $ secret) <> "," <> (T.pack . show $ num ) <> "): marking as read ...."
       markRead <|> lift (throwServer (MessageAlreadyGone num))
     pure ()
+
+-- Internal function for debugging:
+prettyKey :: Key a -> Text
+prettyKey = T.pack . show . (unsafeCoerce :: Key a -> Int)
