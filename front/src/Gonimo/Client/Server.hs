5f00671f0f7d85e025833f61ed9e4c0eb492507a
Robert Klotzner <robert.klotzner@gmx.at>
2016-12-30 08:29:11 +0100

    Server communication yeah!

--- front/src/Gonimo/Client/Server.hs
+++ front/src/Gonimo/Client/Server.hs
@@ -1,22 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Gonimo.Client.Server where
 
 import Gonimo.SocketAPI (ServerRequest, ServerResponse)
 import Reflex
+import Reflex.Class
+import Reflex.Dom.Class
 import Control.Lens
+import Reflex.PerformEvent.Class
+import Reflex.PostBuild.Class
+import Reflex.TriggerEvent.Class
+import qualified Data.Aeson as Aeson
+import GHCJS.DOM.Types (MonadJSM)
+import qualified Data.ByteString.Lazy as BL
+import Reflex.Dom.WebSocket (WebSocketConfig(..), RawWebSocket(..), webSocket)
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import Data.Maybe (fromMaybe)
 
-data ServerConfig t
-  = ServerConfig { _serverConfigSend :: Event t [ServerRequest]
-                 }
-
-makeLenses ''ServerConfig
-
-data Server t
-  = Server { _serverReceive :: Event t ServerResponse
-           }
-
-server :: forall t m. (MonadJSM m, MonadJSM (Performable m), HasJSContext m, PerformEvent t m, TriggerEvent t m, PostBuild t m, IsWebSocketMessage a) => String -> ServerConfig t -> m (Server t)
-server url config 
+type ServerConfig t = WebSocketConfig t ServerRequest
 
 
-makeLenses ''ServerConfig
-makeLenses ''Server
+type Server t = RawWebSocket t ServerResponse
+
+-- Generate our own lenses, because simle lenses are not good enough!
+makeLenses ''WebSocketConfig
+makeLenses ''RawWebSocket
+
+server :: forall t m. (MonadJSM m, MonadJSM (Performable m), HasJSContext m, PerformEvent t m, TriggerEvent t m, PostBuild t m) => Text -> ServerConfig t -> m (Server t)
+server url config = do
+  ws <- webSocket url
+        $ config & webSocketConfig_send . mapped . mapped %~ BL.toStrict . Aeson.encode
+  pure $ ws & webSocket_recv . mapped %~ decodeResponse
+
+
+decodeResponse :: ByteString -> ServerResponse
+decodeResponse = fromMaybe (error "Decoding Server Response Failed!") . Aeson.decodeStrict
+
