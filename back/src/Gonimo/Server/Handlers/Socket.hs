4fa1c4390b8a26c07b5cec862de06a004be30a94
Robert Klotzner <robert.klotzner@gmx.at>
2017-01-11 09:25:15 +0100

    Security: Fixed Auth check for sending messages!

--- back/src/Gonimo/Server/Handlers/Socket.hs
+++ back/src/Gonimo/Server/Handlers/Socket.hs
@@ -6,40 +6,44 @@ import           Gonimo.Server.Effects
 import           Gonimo.Server.Error
 import           Gonimo.Server.Messenger
 import           Gonimo.Types
-import           Gonimo.Db.Entities            (FamilyId, DeviceId)
+import           Gonimo.Db.Entities            (DeviceId)
 import           Control.Monad.IO.Class (liftIO)
 
 -- | Create a channel for communication with  a baby station
 createChannelR :: (AuthReader m, MonadServer m)
-              => FamilyId -> DeviceId -> DeviceId -> m Secret
-createChannelR familyId fromId toId = do
-  authorizeAuthData $ isDevice fromId
-  authorizeAuthData $ isFamilyMember familyId
-
+              => DeviceId -> DeviceId -> m Secret
+createChannelR fromId toId = do
   secret <- generateSecret
-  sendMessage familyId toId $ MessageCreateChannel fromId secret
+  sendMessage fromId toId $ MessageCreateChannel fromId secret
   return secret
 
 
 sendMessageR :: forall m. (AuthReader m, MonadServer m)
-           => FamilyId -> DeviceId -> DeviceId -> Secret -> Text -> m ()
-sendMessageR familyId fromId toId secret txt = do
-    authorizeAuthData (isFamilyMember familyId)
-    authorizeAuthData ((fromId ==) . deviceKey)
-
-    sendMessage familyId toId $ MessageSendMessage fromId secret txt
+           => DeviceId -> DeviceId -> Secret -> Text -> m ()
+sendMessageR fromId toId secret txt
+  = sendMessage fromId toId $ MessageSendMessage fromId secret txt
 
 
 -- Internal helper function:
-sendMessage :: MonadServer m => FamilyId -> DeviceId -> Message -> m ()
-sendMessage familyId toId msg = do
-  messenger <- getMessenger
-  (mFamily, mSend) <- atomically $ do
-    mSend' <- getReceiverSTM messenger toId
-    mFamily' <- getReceiverFamilySTM messenger toId
-    pure (mFamily', mSend')
+sendMessage :: (AuthReader m, MonadServer m) => DeviceId -> DeviceId -> Message -> m ()
+sendMessage fromId toId msg = do
+  authorizeAuthData $ isDevice fromId
 
-  authorize (Just familyId ==) mFamily -- Ensure that only devices that are seen as online can send messages!
+  messenger <- getMessenger
+  (mFromFamily, mToFamily, mSend) <- atomically $ do
+    mFromFamily' <- getReceiverFamilySTM messenger fromId
+    mToFamily' <- getReceiverFamilySTM messenger toId
+
+    mSend' <- getReceiverSTM messenger toId
+    pure (mFromFamily', mToFamily', mSend')
+
+  -- Both devices have to be online:
+  fromFamily <- authorizeJust id mFromFamily
+  toFamily   <- authorizeJust id mToFamily
+  -- and online in the same family:
+  authorize (fromFamily ==) toFamily
+  -- Additional sanity check:
+  authorizeAuthData $ isFamilyMember fromFamily
   case mSend of
     Nothing -> throwServer DeviceOffline
     Just send -> liftIO $ send msg
