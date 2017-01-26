f0ed3da42504bcfa869a2d3c7da8b539d90ac04d
Robert Klotzner <robert.klotzner@gmx.at>
2017-01-26 18:40:14 +0100

    Made config more patch friendly

--- front/src/Gonimo/Client/Config.hs
+++ front/src/Gonimo/Client/Config.hs
@@ -4,8 +4,16 @@ module Gonimo.Client.Config where
 import Data.Text (Text)
 import Data.Monoid
 
+secure :: Bool
+secure = False
+
+prefix :: Text
+prefix = if secure
+         then "wss://"
+         else "ws://"
+
 gonimoBackServer :: Text
 gonimoBackServer = "localhost:8081"
 
 gonimoBackWSURL :: Text
-gonimoBackWSURL = "wss://" <> gonimoBackServer
+gonimoBackWSURL = prefix <> gonimoBackServer
