e4d1b86c67076db4af6a00203da3dad04762a0c5
Robert Klotzner <robert.klotzner@gmx.at>
2017-01-30 10:34:03 +0100

    Minify and compress steps for production

--- front/Setup.hs
+++ front/Setup.hs
@@ -1,20 +1,32 @@
 import Distribution.Simple
-import Distribution.Simple.Setup (BuildFlags(..))
-import Distribution.PackageDescription (PackageDescription (..))
+import Distribution.Simple.Setup (BuildFlags(..), ConfigFlags(..))
+import Distribution.PackageDescription (PackageDescription (..), FlagName(..))
 import Distribution.Simple.LocalBuildInfo (LocalBuildInfo (..))
 import Distribution.Simple.Program.Run
 import Distribution.Verbosity (normal)
 
 finishBuild ::  Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
-finishBuild args buildFlags packageDescription localBuildInfo = do
+finishBuild _ _ _ localBuildInfo = do
+  let flags = configConfigurationsFlags . configFlags $ localBuildInfo
+  let unFlagName = \(FlagName s) -> s
+  let flagStrings = unFlagName . fst <$> filter snd flags
   let prog = emptyProgramInvocation { progInvokePath = "./postBuild.sh"
+                                    , progInvokeArgs = flagStrings
                                     -- , progInvokeArgs = [ "-a", "./static/*", "./dist/build/gonimo-front/gonimo-front.jsexe/" ]
                                     -- , progInvokeCwd = Just "./"
                                     }
   runProgramInvocation normal prog
   -- _ <- system "cp -a static/* dist/build/gonimo-front/gonimo-front.jsexe/"
 
+cleanBuild ::  Args -> BuildFlags -> IO ()
+cleanBuild _ _ = do
+  let prog = emptyProgramInvocation { progInvokePath = "./preBuild.sh"
+                                    }
+  runProgramInvocation normal prog
+  pure (Nothing, [])
 
 main = do
   putStrLn "In Setup.hs"
-  defaultMainWithHooks $ simpleUserHooks { postBuild = finishBuild }
+  defaultMainWithHooks $ simpleUserHooks { postBuild = finishBuild
+                                         , preBuild = cleanBuild
+                                         }
--- front/postBuild.sh
+++ front/postBuild.sh
@@ -1,2 +1,30 @@
 #!/usr/bin/env bash
-cp -a static/* dist/build/gonimo-front/gonimo-front.jsexe/
+dev=$1
+distPath=dist/build/gonimo-front/gonimo-front.jsexe
+cp -a static/* ${distPath}/
+gonimo-deploy md5sum ${distPath}
+if [[ ${dev} == "dev" ]]
+then
+    echo "Development build, skipping minification"
+else
+    pushd ${distPath}
+    echo 'window["h$mainZCZCMainzimain"] = h$mainZCZCMainzimain;' >> out.js
+    echo 'window["h$main"] = h$main;' >> out.js
+    grep -q window runmain.js
+    if [[ $? == 0 ]]
+    then
+        echo "runmain already fixed up ..."
+    else
+        sed -i 's/h$main/window.h$main/g' runmain.js
+    fi
+    cat rts.js lib.js out.js > all.js
+    # closure-compiler all.js --compilation_level=ADVANCED_OPTIMIZATIONS  > all.min.js
+    closure-compiler all.js --compilation_level=SIMPLE_OPTIMIZATIONS  > all.min.js
+    gzip -f -9 all.min.js
+    cat index.html | grep -v rts.js | grep -v lib.js | sed 's/out.js/all.min.js/1' > index-new.html
+    rm index.html
+    mv index-new.html index.html
+    #cleanup:
+    rm rts.js lib.js out.js all.js manifest.webapp out.stats
+    popd
+fi
