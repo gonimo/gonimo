45b06768c43a0e34fdca9a0c12ee136c2db40497
Robert Klotzner <robert.klotzner@gmx.at>
2017-02-03 07:29:36 +0100

    Group by Account

--- front/src/Gonimo/Client/DeviceList/UI.hs
+++ front/src/Gonimo/Client/DeviceList/UI.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE TupleSections #-}
 module Gonimo.Client.DeviceList.UI where
 
 import Reflex.Dom
@@ -24,7 +25,7 @@ import qualified Gonimo.Client.Storage.Keys as GStorage
 import qualified GHCJS.DOM as DOM
 import Data.Foldable (traverse_)
 import Gonimo.Client.Reflex
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, catMaybes)
 import qualified Gonimo.Types as Gonimo
 import qualified Gonimo.Client.Invite as Invite
 import Control.Monad.IO.Class (liftIO)
@@ -58,26 +59,63 @@ renderList config deviceList' = do
       el "th" blank
       el "th" blank
       el "th" blank
-    el "tbody" $ dyn $ renderRows <$> deviceList'^.deviceIds
-                                  <*> deviceList'^.deviceInfos
-                                  <*> pure (deviceList'^.onlineDevices) -- don't re-render full table
-                                  <*> config^.configAuthData
+    tz <- liftIO $ getCurrentTimeZone
+    el "tbody" $ dyn $ renderAccounts tz <$> deviceList'^.deviceIds
+                                         <*> deviceList'^.deviceInfos
+                                         <*> pure (deviceList'^.onlineDevices) -- don't re-render full table
+                                         <*> config^.configAuthData
   pure ()
 
-
-renderRows :: forall m t. (HasWebView m, MonadWidget t m)
-              => Map AccountId (Dynamic t [DeviceId])
+renderAccounts :: forall m t. (HasWebView m, MonadWidget t m)
+              => TimeZone
+              -> Map AccountId (Dynamic t [DeviceId])
               -> Map DeviceId (Dynamic t (API.DeviceInfo))
               -> Dynamic t (Map DeviceId DeviceType)
               -> Maybe API.AuthData
               -> m ()
-renderRows deviceIds' infos onlineStatus mAuthData = do
-  tz <- liftIO $ getCurrentTimeZone
+renderAccounts tz deviceIds' infos onlineStatus mAuthData = do
+  let
+    isSelf (aId, info') = Just aId == (API.accountId <$> mAuthData)
+    (self, others) = List.partition isSelf . Map.toList $ deviceIds'
+  traverse_ (uncurry (renderAccount tz "info" infos onlineStatus mAuthData)) self
+  traverse_ (uncurry (renderAccount tz "" infos onlineStatus mAuthData)) others
+
+-- Currently pretty dumb, once we have non anonymous accounts render those differently:
+-- Visible group non devices belonging to a single account. Also family removal is per account.
+renderAccount :: forall m t. (HasWebView m, MonadWidget t m)
+              => TimeZone
+              -> Text
+              -> Map DeviceId (Dynamic t (API.DeviceInfo))
+              -> Dynamic t (Map DeviceId DeviceType)
+              -> Maybe API.AuthData
+              -> AccountId
+              -> Dynamic t [DeviceId]
+              -> m ()
+renderAccount tz _ infos onlineStatus mAuthData  accountId' devIds'= do
+  let
+    filterInfos infos' devIds =
+      let
+        mMapValues = flip Map.lookup infos' <$> devIds
+        mEntries = zipWith (\k mv -> (k,) <$> mv) devIds mMapValues
+      in
+        Map.fromList . catMaybes $ mEntries
+    filteredInfos = filterInfos infos <$> devIds'
+  dyn $ renderRows <$> pure tz <*> filteredInfos <*> pure onlineStatus <*> pure mAuthData
+  pure ()
+
+renderRows :: forall m t. (HasWebView m, MonadWidget t m)
+              => TimeZone
+              -> Map DeviceId (Dynamic t (API.DeviceInfo))
+              -> Dynamic t (Map DeviceId DeviceType)
+              -> Maybe API.AuthData
+              -> m ()
+renderRows tz infos onlineStatus mAuthData = do
   let
     isSelf (devId, info') = Just devId == (API.deviceId <$> mAuthData)
     (self, others) = List.partition isSelf . Map.toList $ infos
   traverse_ (uncurry (renderRow tz "info" onlineStatus)) self
   traverse_ (uncurry (renderRow tz "" onlineStatus)) others
+
 
 renderRow :: forall m t. (HasWebView m, MonadWidget t m)
               => TimeZone -> Text -> Dynamic t (Map DeviceId DeviceType)
