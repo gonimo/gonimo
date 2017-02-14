4395bcdc72fea9238a7cb776f214563623a8826b
Robert Klotzner <robert.klotzner@gmx.at>
2017-02-14 10:00:46 +0100

    Added more imports to common Prelude

--- common/src/Gonimo/Prelude.hs
+++ common/src/Gonimo/Prelude.hs
@@ -4,9 +4,19 @@ module Gonimo.Prelude ( MaybeT(..)
                       , liftIO
                       , lift
                       , traverse_
+                      , module Maybe
+                      , module Safe
+                      , module Monoid
+                      , module Applicative
+                      , Text
                       ) where
 
 import Control.Monad.Trans.Maybe (MaybeT(..))
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Class (lift)
 import Data.Foldable (traverse_)
+import Data.Maybe as Maybe (fromMaybe, maybe, isJust, isNothing)
+import Safe as Safe (headMay)
+import Data.Text (Text)
+import Data.Monoid as Monoid
+import Control.Applicative as Applicative
