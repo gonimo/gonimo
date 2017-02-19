922b0d4cbf1c7d1ff0f3ad9f0f565ff20c5b9e86
Robert Klotzner <robert.klotzner@gmx.at>
2017-02-19 10:50:48 +0100

    Generate default.nix in workOn.sh scripts

--- back/workOn.sh
+++ back/workOn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+cabal2nix --no-haddock --no-check -fdev ./ > default.nix
 nix-shell -E "let this = import ../../gonimo-server/pkgs/alpha/. {pkgs = (import <nixpkgs> {}).pkgs;};in this.gonimo-back-dev.env"
