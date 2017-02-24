c2b29f413eb37c095c74138d07a0f16ea3f50932
Robert Klotzner <robert.klotzner@gmx.at>
2017-02-24 13:41:51 +0100

    Quickndirty- make workOn.sh script "pure"

--- front/workOn.sh
+++ front/workOn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cabal2nix --no-haddock --no-check -fdev ./ > default.nix
-nix-shell -E "let this = import ../../gonimo-server/pkgs/alpha/. {pkgs = (import <nixpkgs> {}).pkgs;};in this.gonimo-front-dev.env"
+nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/af3732b6c6adb11f45cc9d72c3ddf33fd51c47e0.tar.gz -E "let this = import ../../gonimo-server/pkgs/alpha/. {pkgs = (import <nixpkgs> {}).pkgs;};in this.gonimo-front-dev.env" $@
