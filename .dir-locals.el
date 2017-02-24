70aa9caf712f73af33b1259a9607ca0124c7fbfe
Robert Klotzner <robert.klotzner@gmx.at>
2017-02-24 14:28:55 +0100

    Make cabal build command nix-shell aware :-)
    Yeah!

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,6 @@
 ;; (
 ;;  (haskell-mode . ((haskell-process-args-stack-ghci . ("--ghc-options=-ferror-spans" "gonimo-back:exe:gonimo-back"))))
 ;;  )
+((haskell-mode . ((haskell-compile-cabal-build-command . "cd %s && ../runIn.sh cabal build --ghc-option=-ferror-spans")
+                  ))
+ )
