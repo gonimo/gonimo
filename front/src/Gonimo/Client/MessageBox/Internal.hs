6d7b3f89b3e5b14a034dd942cac20fe2eac3cccf
Robert Klotzner <robert.klotzner@gmx.at>
2017-02-27 13:30:57 +0100

    Import cleanup

--- front/src/Gonimo/Client/MessageBox/Internal.hs
+++ front/src/Gonimo/Client/MessageBox/Internal.hs
@@ -6,32 +6,11 @@
 module Gonimo.Client.MessageBox.Internal where
 
 import Reflex.Dom
-import Control.Monad
-import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Lens
-import Data.Monoid
 import Data.Text (Text)
-import Gonimo.Db.Entities (FamilyId, InvitationId)
-import Gonimo.Types (Secret)
-import qualified Gonimo.Db.Entities as Db
-import qualified Gonimo.SocketAPI.Types as API
-import qualified Gonimo.SocketAPI as API
-import qualified GHCJS.DOM.Location as Location
-import qualified GHCJS.DOM.History as History
-import GHCJS.DOM.Types (ToJSVal, toJSVal, FromJSVal, fromJSVal, JSVal)
-import qualified GHCJS.DOM.Window as Window
-import qualified GHCJS.DOM as DOM
-import Control.Monad.Trans.Maybe
-import Data.Maybe (maybe)
-import qualified Data.Aeson as Aeson
-import qualified Data.Text.Encoding as T
-import qualified Data.Text as T
-import qualified Data.ByteString.Lazy as BL
-import Network.HTTP.Types (urlDecode)
-import Control.Monad.Fix (MonadFix)
-import qualified Data.Aeson as Aeson
-import Gonimo.SocketAPI.Types (InvitationReply)
 import qualified Gonimo.Client.App.Types as App
+import qualified Gonimo.SocketAPI as API
+import Gonimo.Db.Entities (FamilyId)
 import Gonimo.Client.Server (webSocket_recv)
 
 invitationQueryParam :: Text
