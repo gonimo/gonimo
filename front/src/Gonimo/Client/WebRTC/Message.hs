c83089e0a9862fe3b9af5f3d532bf24bcb7ccc7a
Robert Klotzner <robert.klotzner@gmx.at>
2017-03-03 13:36:47 +0100

    Debugging output (commented)

--- front/src/Gonimo/Client/WebRTC/Message.hs
+++ front/src/Gonimo/Client/WebRTC/Message.hs
@@ -6,13 +6,13 @@
 
 module Gonimo.Client.WebRTC.Message where
 
+import           Debug.Trace                     (trace)
 import           GHCJS.DOM.RTCIceCandidate
 import           GHCJS.DOM.RTCSessionDescription
-import           GHCJS.DOM.Types             (Dictionary (..), MonadJSM)
-import qualified Gonimo.SocketAPI.Types      as API
-import           Language.Javascript.JSaddle (liftJSM, (<#))
-import qualified Language.Javascript.JSaddle as JS
-
+import           GHCJS.DOM.Types                 (Dictionary (..), MonadJSM)
+import qualified Gonimo.SocketAPI.Types          as API
+import           Language.Javascript.JSaddle     (liftJSM, (<#))
+import qualified Language.Javascript.JSaddle     as JS
 
 toJSIceCandidate :: MonadJSM m => API.IceCandidate -> m RTCIceCandidate
 toJSIceCandidate (API.IceCandidate index mid candidate) = liftJSM $ do
@@ -21,6 +21,8 @@ toJSIceCandidate (API.IceCandidate index mid candidate) = liftJSM $ do
   rawDic <# "sdpMid" $ mid
   rawDic <# "candidate" $ candidate
   let wrappedDic = case rawDic of JS.Object val -> Dictionary val
+  -- printObj <- JS.eval $ "(function(obj) { \"toJS candidate: \" + console.log(JSON.stringify(obj, null, 4));;})"
+  -- _ <- JS.call printObj JS.obj [rawDic]
   newRTCIceCandidate $ Just wrappedDic
 
 fromJSIceCandidate :: MonadJSM m => RTCIceCandidate -> m API.IceCandidate
@@ -33,6 +35,9 @@ fromJSIceCandidate (RTCIceCandidate jsCandidate) = liftJSM $ do
   -- candidate <- getCandidate jsCandidate
   -- mid <- getSdpMid jsCandidate
   -- index <- fromIntegral <$> getSdpMLineIndex jsCandidate
+
+  -- printObj <- JS.eval $ "(function(obj) { \"fromJS candidate: \" + console.log(JSON.stringify(obj, null, 4));;})"
+  -- _ <- JS.call printObj JS.obj [jsCandidate]
   pure $ API.IceCandidate sdpMLineIndex mid candidate
 
 toJSSessionDescription :: MonadJSM m => API.SessionDescription -> m RTCSessionDescription
@@ -41,10 +46,14 @@ toJSSessionDescription (API.SessionDescription sdp type_) = liftJSM $ do
   rawDic <# "sdp" $ sdp
   rawDic <# "type" $ type_
   let wrappedDic = case rawDic of JS.Object val -> Dictionary val
+  -- printObj <- JS.eval $ "(function(obj) { \"toJS session description: \" + console.log(JSON.stringify(obj, null, 4));;})"
+  -- _ <- JS.call printObj JS.obj [rawDic]
   newRTCSessionDescription $ Just wrappedDic
 
 fromJSSessionDescription :: MonadJSM m => RTCSessionDescription -> m API.SessionDescription
 fromJSSessionDescription jsSession = liftJSM $ do
   sdp <- getSdp jsSession
   type_ <- getType jsSession
+  -- printObj <- JS.eval $ "(function(obj) { \"fromJS session description: \" + console.log(JSON.stringify(obj, null, 4));;})"
+  -- _ <- JS.call printObj JS.obj [jsSession]
   pure $ API.SessionDescription sdp type_
