69702ce3ba8006efbb88a988d1a883456a2d34d5
Robert Klotzner <robert.klotzner@gmx.at>
2017-03-28 18:37:28 +0200

    Updated nixpkgs

--- back/workOn.sh
+++ back/workOn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cabal2nix --no-haddock --no-check -fdev ./ > default.nix
-nix-shell --pure -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/af3732b6c6adb11f45cc9d72c3ddf33fd51c47e0.tar.gz -E "let this = import ../../gonimo-server/pkgs/alpha/. {pkgs = (import <nixpkgs> {}).pkgs;};in this.gonimo-back-dev.env" "$@"
+nix-shell --pure -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/ab1078806ecf7f1ef28b3a0bd7cda1e9af8e7875.tar.gz -E "let this = import ../../gonimo-server/pkgs/alpha/. {pkgs = (import <nixpkgs> {}).pkgs;};in this.gonimo-back-dev.env" "$@"
