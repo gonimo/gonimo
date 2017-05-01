a5d3f4c1d1506492b4390336822a95ecf7b9913f
Robert Klotzner <robert.klotzner@gmx.at>
2017-05-01 16:25:58 +0200

    Derive FromJSON/ToJSON Generic for Locale

--- common/src/Gonimo/I18N.hs
+++ common/src/Gonimo/I18N.hs
@@ -1,10 +1,18 @@
 module Gonimo.I18N where
 
 import Data.Text
+import           Data.Aeson.Types      (FromJSON, ToJSON (..), defaultOptions,
+                                        genericToJSON, genericToEncoding)
+import GHC.Generics
 
 data Locale = DE_DE
             | EN_GB
-            deriving (Show, Eq)
+            deriving (Show, Eq, Generic)
+
+instance FromJSON Locale
+instance ToJSON Locale where
+  toJSON = genericToJSON defaultOptions
+  toEncoding = genericToEncoding defaultOptions
 
 class I18N msg where
   {-# MINIMAL i18n #-}
