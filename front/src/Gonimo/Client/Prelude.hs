7a22cdad0063aae5b3f70bc8f8e10fa9f8c33c31
Robert Klotzner <robert.klotzner@gmx.at>
2017-05-01 13:44:14 +0200

    Reader monad for translation

--- front/src/Gonimo/Client/Prelude.hs
+++ front/src/Gonimo/Client/Prelude.hs
@@ -3,6 +3,7 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 module Gonimo.Client.Prelude ( MonadFix
                              , module GonimoPrelude
+                             , module I18N
                              ) where
 
 import           Control.Monad.Fix         (MonadFix)
@@ -12,9 +13,19 @@ import           Control.Monad.Trans.Maybe (MaybeT)
 import           GHCJS.DOM.Types           (MonadJSM (..), liftJSM)
 #endif
 import           Gonimo.Prelude as GonimoPrelude
+import           Reflex.Dom.Core
+import           Gonimo.Client.I18N as I18N (trText, trDynText)
+import           Gonimo.Client.I18N (GonimoEnv)
+import Control.Monad.Reader.Class
 
 -- Only needed on GHC, because on GHCJS MonadJSM is MonadIO
 #ifndef __GHCJS__
 instance MonadJSM m => MonadJSM (MaybeT m) where
   liftJSM' = lift . liftJSM
 #endif
+
+
+type GonimoM t m = ( DomBuilder t m
+                   , PostBuild t m
+                   , MonadReader (GonimoEnv t) m
+                   )
