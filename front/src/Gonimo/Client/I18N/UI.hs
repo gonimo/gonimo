30469d5ed223fedacb55e443e6419c4d55fce339
Robert Klotzner <robert.klotzner@gmx.at>
2017-05-02 18:33:16 +0200

    Added missing file

---
+++ front/src/Gonimo/Client/I18N/UI.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE RecursiveDo #-}
+module Gonimo.Client.I18N.UI where
+
+import Gonimo.Client.Prelude
+import Control.Monad.Reader.Class
+import Reflex.Dom.Core
+import Gonimo.Client.I18N
+import Gonimo.I18N
+import Gonimo.Client.Reflex.Dom
+
+
+langSelector :: forall t m. GonimoM t m => m (Event t Locale)
+langSelector = elClass "div" "lang-select" $ mdo
+    locDyn <- asks _gonimoLocale
+    let flagClass = pure "flag " <> fmap localeCssClass locDyn
+    clicked <-
+      makeClickable . elDynAttr' "div" (addBtnDynAttrs flagClass) $ blank
+    pure $ pushAlways (const $ do
+                          cLoc <- sample $ current locDyn
+                          pure $ case cLoc of
+                                   DE_DE -> EN_GB
+                                   EN_GB -> DE_DE
+                      ) clicked
+  --   let openClose = pushAlways (\_ -> not <$> sample (current droppedDown)) clicked
+  --   droppedDown <- holdDyn False $ leftmost [ openClose
+  --                                           , const False <$> selectedName
+  --                                           ]
+  --   let
+  --     droppedDownClass :: Dynamic t Text
+  --     droppedDownClass = fmap (\opened -> if opened then "isDroppedDown " else "") droppedDown
+  --   let
+  --     dropDownClass :: Dynamic t Text
+  --     dropDownClass = pure "dropDown-container " <> droppedDownClass
+
+  --   selectedName <-
+  --     elDynClass "div" dropDownClass $ renderLangSelectors [DE_DE, EN_GB]
+  --   pure selectedName
+  -- where
+
+  --   renderLangSelectors :: [Locale] -> m (Event t Locale)
+  --   renderLangSelectors langs =
+  --     elClass "div" "" $
+  --       leftmost <$> traverse renderLangSelector langs
+
+  --   renderLangSelector :: Locale -> m (Event t Locale)
+  --   renderLangSelector label = do
+  --       clicked <-
+  --         makeClickable
+  --         . elAttr' "div" (addBtnAttrs $ "flag " <> localeCssClass label) $ blank
+  --       pure $ const label <$> clicked
+
+localeCssClass :: Locale -> Text
+localeCssClass loc
+  = case loc of
+      DE_DE -> "flag-de"
+      EN_GB -> "flag-gb"
