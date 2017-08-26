113ebb1839d69f2afc212cfc31637f6d5639a966
Robert Klotzner <robert.klotzner@gmx.at>
2017-08-26 08:27:23 +0200

    Use nixpkgs url from gonimo-server

--- scripts/workOn.sh
+++ scripts/workOn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 NIXOPTS="--option extra-binary-caches https://nixcache.reflex-frp.org -j 8"
-NIX_PATH="nixpkgs-overlays=../../gonimo-server/pkgs" nix-shell $NIXOPTS --pure -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/8e75b4dc7b1553026c70d95a34c408dabb852943.tar.gz -E "with import <nixpkgs> {}; pkgs.gonimo.alpha.${pkg}.env" "$@"
+NIX_PATH="nixpkgs-overlays=../../gonimo-server/pkgs" nix-shell $NIXOPTS --pure -I nixpkgs=$(cat ../../gonimo-server/gonimo-nixpkgs-url) -E "with import <nixpkgs> {}; pkgs.gonimo.alpha.${pkg}.env" "$@"
