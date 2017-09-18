3495eeaa64114292e3a3cdd7465ee7976539870a
Robert Klotzner <robert.klotzner@gmx.at>
2017-09-18 10:58:55 +0200

    Added build.gonimo.com to binary caches

--- scripts/workOn.sh
+++ scripts/workOn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-NIXOPTS="--option extra-binary-caches https://nixcache.reflex-frp.org -j 8"
+NIXOPTS="--option extra-binary-caches https://build.gonimo.com --option extra-binary-caches https://nixcache.reflex-frp.org -j 8"
 NIX_PATH="nixpkgs-overlays=../../gonimo-server/pkgs" nix-shell $NIXOPTS --pure -I nixpkgs=$(cat ../../gonimo-server/gonimo-nixpkgs-url) -E "with import <nixpkgs> {}; pkgs.gonimo.alpha.${pkg}.env" "$@"
