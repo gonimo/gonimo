bb1bf29f4de92a09acdb15c18fa2b9b5d3dccbb8
Robert Klotzner <robert.klotzner@gmx.at>
2017-11-01 10:37:21 +0100

    Fixed warnings

--- back/src/Gonimo/Server/Messenger.hs
+++ back/src/Gonimo/Server/Messenger.hs
@@ -27,22 +27,22 @@ import Gonimo.Server.Messenger.Internal as I ( FromId
 import Gonimo.Server.Messenger.Internal
 
 registerReceiverSTM :: MessengerVar -> DeviceId -> (Message -> IO ()) -> STM (Maybe (Message -> IO ()))
-registerReceiverSTM mesVar deviceId receiver = runStateSTM mesVar $ registerReceiver deviceId receiver
+registerReceiverSTM mesVar deviceId' receiver = runStateSTM mesVar $ registerReceiver deviceId' receiver
 
 setDeviceTypeSTM :: MessengerVar -> DeviceId -> DeviceType -> STM ()
-setDeviceTypeSTM mesVar deviceId deviceType = runStateSTM mesVar $ setDeviceType deviceId deviceType
+setDeviceTypeSTM mesVar deviceId' deviceType = runStateSTM mesVar $ setDeviceType deviceId' deviceType
 
 getOnlineDevicesSTM :: MessengerVar -> FamilyId -> STM [(DeviceId, DeviceType)]
 getOnlineDevicesSTM mesVar familyId = getOnlineDevices familyId <$> readTVar mesVar
 
 getReceiverSTM :: MessengerVar -> DeviceId ->  STM (Maybe (Message -> IO ()))
-getReceiverSTM mesVar deviceId = getReceiver deviceId <$> readTVar mesVar
+getReceiverSTM mesVar deviceId' = getReceiver deviceId' <$> readTVar mesVar
 
 getReceiverFamilySTM :: MessengerVar -> DeviceId ->  STM (Maybe FamilyId)
-getReceiverFamilySTM mesVar deviceId = getReceiverFamily deviceId <$> readTVar mesVar
+getReceiverFamilySTM mesVar deviceId' = getReceiverFamily deviceId' <$> readTVar mesVar
 
 switchFamilySTM :: MessengerVar -> DeviceId -> FamilyId -> STM ()
-switchFamilySTM mesVar deviceId familyId = runStateSTM mesVar $ switchFamily deviceId familyId
+switchFamilySTM mesVar deviceId' familyId' = runStateSTM mesVar $ switchFamily deviceId' familyId'
 
 deleteReceiverSTM :: MessengerVar -> DeviceId -> STM ()
 deleteReceiverSTM mesVar = runStateSTM mesVar . deleteReceiver
