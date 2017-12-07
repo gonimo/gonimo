e3ffef287186593e3c1bee98326b367abcf661c7
Robert Klotzner <robert.klotzner@gmx.at>
2017-12-07 22:31:48 +0100

    Authorize conceptually finished.
    Nothing compiles yet!

--- back/src/Gonimo/Server/Authorize/Internal.hs
+++ back/src/Gonimo/Server/Authorize/Internal.hs
@@ -26,10 +26,6 @@ data AuthRequest
 make :: forall t m. MonadAppHost t m => Config t -> m (Authorize t)
 make config = undefined
 
-data Authorized
-  = Authorized DeviceId FromClient
-  | Denied DeviceId ToClient
-
 -- | Tell what requests should be forbidden.
 --
 --   Returns a 'Just' 'ServerError' for requests that should not be allowed.
@@ -58,39 +54,39 @@ deny auth@AuthRequest {..} =
 denyUpdate :: AuthRequest -> Update -> Maybe ServerError
 denyUpdate auth@AuthRequest {..} update' =
   case udpate' of
-    OnChangedFamilyName         fid name  -> denyUnless (isOnlineInFamily clients' senderId fid)
-    OnChangedFamilyLastAccessed fid time' -> pure Forbidden
-    OnNewFamilyMember           fid aid -> pure Forbidden
-    OnRemovedFamilyMember       fid aid -> denyUnless (isOnlineInFamily clients' senderId fid)
-    OnNewFamilyInvitation       fid invId -> pure Forbidden
-    OnRemovedFamilyInviation fid invId -> denyUnless ( isOnlineInFamily clients' senderId fid
+    OnChangedFamilyName         fid name    -> denyUnless (isOnlineInFamily clients' senderId fid)
+    OnChangedFamilyLastAccessed fid time'   -> pure Forbidden
+    OnNewFamilyMember           fid aid     -> pure Forbidden
+    OnRemovedFamilyMember       fid aid     -> denyUnless (isOnlineInFamily clients' senderId fid)
+    OnNewFamilyInvitation       fid invId   -> pure Forbidden
+    OnRemovedFamilyInviation fid invId      -> denyUnless ( isOnlineInFamily clients' senderId fid
                                                        && onlineFamilyHasInvitation auth invId
                                                      )
-    OnNewAccountDevice          aid devId -> pure Forbidden
-    OnRemovedAccountDevice      aid devId -> pure Forbidden
-    OnNewAccountInvitation      aid invId -> pure Forbidden
-    OnNewAccountFamily          aid fid -> pure Forbidden
-    OnChangedDeviceName         devId _ -> denyUnless (senderId == devId)
+    OnNewAccountDevice          aid devId   -> pure Forbidden
+    OnRemovedAccountDevice      aid devId   -> pure Forbidden
+    OnNewAccountInvitation      aid invId   -> pure Forbidden
+    OnNewAccountFamily          aid fid     -> pure Forbidden
+    OnChangedDeviceName         devId _     -> denyUnless (senderId == devId)
     OnChangedDeviceLastAccessed devId time' -> pure Forbidden
     OnChangedDeviceStatus       devId fid _ -> denyUnless ( senderId == devId
                                                             && isFamilyMember cache' senderId fid
                                                           )
-    OnClaimedInvitation         invId -> pure Forbidden
-    OnChangedInvitationDelivery invId _ -> denyUnless (onlineFamilyHasInvitation auth invId)
+    OnClaimedInvitation         invId       -> pure Forbidden
+    OnChangedInvitationDelivery invId _     -> denyUnless (onlineFamilyHasInvitation auth invId)
 
 -- | Deny not allowed views.
 denyView :: AuthRequest -> ViewSelector -> Maybe ServerError
 denyView auth@AuthRequest {..} view' =
   case view' of
-   SelectAccountOwn -> mzero -- One is always allowed to retrieve it's own account data.
-   SelectAccountDevice devId -> denyUnless (devId == senderId) -- TODO: Once we have user accounts this check is no longer correct.
-   SelectAccountFamily fid -> denyUnless (isFamilyMember cache' senderId fid)
+   SelectAccountOwn              -> mzero -- One is always allowed to retrieve it's own account data.
+   SelectAccountDevice devId     -> denyUnless (isSameAccount auth devId)
+   SelectAccountFamily fid       -> denyUnless (isFamilyMember cache' senderId fid)
    SelectAccountInvitation invId -> denyUnless (hasClaimedInvitation cache' senderId invId)
 
-  | SelectFamily fid -> denyUnless (isOnlineInFamily clients' senderId fid)
-  | SelectFamilyAccount aid -> denyUnless (isAccountInOurFamily auth aid)
-  | SelectFamilyDevice devId -> denyUnless (isDeviceInOurFamily aut devId)
-  | SelectFamilyInvitation !InvitationId
+   SelectFamily fid             -> denyUnless (isOnlineInFamily clients' senderId fid)
+   SelectFamilyAccount aid      -> denyUnless (isAccountInOurFamily auth aid)
+   SelectFamilyDevice devId     -> denyUnless (isDeviceInOurFamily aut devId)
+   SelectFamilyInvitation invId -> denyUnless (familyHasInvitation auth invId)
 
 -- | Return 'Forbidden' unless the first argument is true.
 denyUnless :: MonadPlus m => Bool -> m ServerError
@@ -105,12 +101,25 @@ isFamilyMember cache' senderId' fid = maybe False (const True) $ do
   fids <- cache'^.sampledAccountFamilies.at aid
   mfilter (fid `elem` fids) $ pure ()
 
+-- | Does the given device id belong to the same account as we?
+isSameAccount :: AuthRequest -> DeviceId -> Bool
+isSameAccount AuthRequest {..} other = maybe False (const True) $ do
+  otherDevice <- cache'^.sampledDevices.at devId
+  ourDevice <- cache'^.sampledDevices.at senderId
+  mfilter (devieAccountId ourDevice == deviceAccountId otherDevice) $ pure ()
 
 -- | Does the sender device own the given invitation?
 hasClaimedInvitation :: Cache.Sampled -> DeviceId -> InvitationId -> Bool
 hasClaimedInvitation cache' senderId' invId = maybe False (const True) $ do
   aid <- cache'^?sampledDevices.at senderId'._Just.to deviceAccountId
   invitations <- cache'^.sampledAcountInvitations.at aid
+  mfilter (invId `elem` invitations) $ pure ()
+
+-- | Does the invitatation belong to the family we are currently online in?
+familyHasInvitation :: AuthRequest -> InvitationId -> Bool
+familyHasInvitation AuthRequest {..} invId = maybe False (const True) $ do
+  ourFamily <- clients'^.sampledSelectedFamily . at senderId
+  invitations <- cache'^.sampledFamilyInvitations.at ourFamily
   mfilter (invId `elem` invitations) $ pure ()
 
 -- | Auth check whether a device is currently online in a given family.
