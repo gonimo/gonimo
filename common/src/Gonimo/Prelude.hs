dbde717a6657276f54897f388983f0c7721a3653
Robert Klotzner <robert.klotzner@gmx.at>
2017-12-17 15:04:16 +0100

    Added stuff to our Prelude

--- common/src/Gonimo/Prelude.hs
+++ common/src/Gonimo/Prelude.hs
@@ -11,16 +11,23 @@ module Gonimo.Prelude ( MaybeT(..)
                       , module Monad
                       , Text
                       , module Constants
+                      , module Control.Exception
+                      , module Control.Lens
+                      , module Data.Default
                       ) where
 
 import Control.Monad.Trans.Maybe (MaybeT(..))
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Class (lift)
 import Data.Foldable (traverse_)
-import Data.Maybe as Maybe (fromMaybe, maybe, isJust, isNothing)
+import Data.Maybe as Maybe (fromMaybe, maybe, isJust, isNothing, mapMaybe, catMaybes)
 import Safe as Safe (headMay)
 import Data.Text (Text)
 import Data.Monoid as Monoid
 import Control.Applicative as Applicative
 import Control.Monad as Monad
 import Gonimo.Constants as Constants
+import Control.Arrow as Arrow
+import Control.Exception
+import Control.Lens
+import Data.Default
