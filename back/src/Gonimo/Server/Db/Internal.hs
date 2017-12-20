e42fd795237544dfbfcc2283989fc2fc8dd41ed1
Robert Klotzner <robert.klotzner@gmx.at>
2017-12-20 17:52:59 +0100

    Made Db.Internal compile

--- back/src/Gonimo/Server/Db/Internal.hs
+++ back/src/Gonimo/Server/Db/Internal.hs
@@ -12,11 +12,18 @@ import           Control.Monad.IO.Class           (MonadIO)
 import           Database.Persist.Class           (Key, PersistRecordBackend)
 import qualified Database.Persist.Class           as Db
 import           Database.Persist.Sql             (SqlBackend)
-import           Control.Concurrent.STM.TQueue
+import           Reflex hiding (Request)
 
 import qualified Gonimo.Database.Effects.Servant  as Db
 import           Gonimo.Server.Db.IsDb
 import           Gonimo.Server.Error              (ServerError)
+import           Gonimo.SocketAPI.Model
+import qualified Gonimo.Server.Config as Server
+import qualified Gonimo.Server.Cache as Cache
+
+
+
+
 
 -- | Commands to be performed by the db.
 data Command
@@ -74,3 +81,72 @@ updateRecord noSuchRecord recordId f = do
       let recordId' = toDb recordId
       let f' = stateToDb f
       updateRecord' (noSuchRecord recordId) recordId' f'
+
+-- Lenses stuff:
+
+instance Server.HasConfig (Config t) where
+  config = serverConfig
+
+-- Lenses auto generated:
+
+
+class HasRequest a where
+  request :: Lens' a Request
+
+  requester :: Lens' a DeviceId
+  requester = request . go
+    where
+      go :: Lens' Request DeviceId
+      go f request' = (\requester' -> request' { _requester = requester' }) <$> f (_requester request')
+
+
+  command :: Lens' a Command
+  command = request . go
+    where
+      go :: Lens' Request Command
+      go f request' = (\command' -> request' { _command = command' }) <$> f (_command request')
+
+
+instance HasRequest Request where
+  request = id
+
+class HasConfig a where
+  config :: Lens' (a t) (Config t)
+
+  serverConfig :: Lens' (a t) Server.Config
+  serverConfig = config . go
+    where
+      go :: Lens' (Config t) Server.Config
+      go f config' = (\serverConfig' -> config' { _serverConfig = serverConfig' }) <$> f (_serverConfig config')
+
+
+  onRequest :: Lens' (a t) (Event t [Request])
+  onRequest = config . go
+    where
+      go :: Lens' (Config t) (Event t [Request])
+      go f config' = (\onRequest' -> config' { _onRequest = onRequest' }) <$> f (_onRequest config')
+
+
+instance HasConfig Config where
+  config = id
+
+class HasDb a where
+  db :: Lens' (a t) (Db t)
+
+  onResponse :: Lens' (a t) (Event t Cache.ModelDump)
+  onResponse = db . go
+    where
+      go :: Lens' (Db t) (Event t Cache.ModelDump)
+      go f db' = (\onResponse' -> db' { _onResponse = onResponse' }) <$> f (_onResponse db')
+
+
+  onError :: Lens' (a t) (Event t (Request, ServerError))
+  onError = db . go
+    where
+      go :: Lens' (Db t) (Event t (Request, ServerError))
+      go f db' = (\onError' -> db' { _onError = onError' }) <$> f (_onError db')
+
+
+instance HasDb Db where
+  db = id
+
