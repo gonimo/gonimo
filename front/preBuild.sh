153fc95da82f8776b37afedb7d720211255ee53a
Robert Klotzner <robert.klotzner@gmx.at>
2018-01-22 18:12:03 +0100

    Fix distPath in preBuild.sh

--- front/preBuild.sh
+++ front/preBuild.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 dev=$1
-distPath=dist/build/gonimo-front/gonimo-front.jsexe
+if [[ -d ../dist-ghcjs ]]
+then
+    distPath=../dist-ghcjs/build/x86_64-linux/ghcjs-0.2.1/gonimo-front-0.1/build/gonimo-front/gonimo-front.jsexe
+else
+    distPath=dist/build/gonimo-front/gonimo-front.jsexe
+fi
 [ -d ${distPath} ] && rm -Rf ${distPath}/*
 exit 0
