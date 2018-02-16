a6eb13bcae1c6484c0da91c4d5e28d49db38a6db
Robert Klotzner <robert.klotzner@gmx.at>
2018-02-16 15:23:16 +0100

    Make Locale a Semigroup

--- common/src/Gonimo/I18N.hs
+++ common/src/Gonimo/I18N.hs
@@ -3,11 +3,17 @@ module Gonimo.I18N where
 import Data.Text
 import           Data.Aeson.Types      (FromJSON, ToJSON (..), defaultOptions,
                                         genericToJSON, genericToEncoding)
+
+import Data.Semigroup
 import GHC.Generics
 
 data Locale = DE_DE
             | EN_GB
             deriving (Show, Eq, Generic)
+
+-- Probably not a good idea, but we need it.
+instance Semigroup Locale where
+  a <> _ = a
 
 instance FromJSON Locale
 instance ToJSON Locale where
