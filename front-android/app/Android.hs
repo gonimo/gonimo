27c3a9da62589c3d4022ef566505030ab1bc5d5d
Robert Klotzner <robert.klotzner@gmx.at>
2018-02-18 02:14:34 +0100

    Fixed #222 Invitation links can now be handled in running app

--- front-android/app/Android.hs
+++ front-android/app/Android.hs
@@ -7,9 +7,11 @@
 
 import           Android.HaskellActivity
 import           Control.Concurrent
+import           Control.Concurrent.MVar       (putMVar)
+import           Control.Lens
 import           Control.Monad
-import qualified Data.ByteString               as BS
-import qualified Data.ByteString.Lazy          as BL
+import qualified Data.Aeson                    as Aeson
+import qualified Data.ByteString.UTF8          as BS
 import           Data.Default
 import           Data.Monoid
 import           Data.String
@@ -17,7 +19,6 @@ import           Language.Javascript.JSaddle   (JSM)
 import           Network.HTTP.Types            (urlDecode)
 import           Reflex.Dom.Android.MainWidget
 import           System.IO
-import           Control.Concurrent.MVar (putMVar)
 
 import qualified Gonimo.Client.Main            as Gonimo
 import           Gonimo.Types                  ()
@@ -26,22 +27,23 @@ main :: IO ()
 main = do
   conf <- Gonimo.mkEmptyConfig
   let
+    handleIntent :: String -> IO ()
     handleIntent url = do
+      putStrLn $ "gonimo, handling intent: " <> url
       let
-        query = dropWhile (/= '?') intentData
-        encodedSecret = dropWhile (/= '=') query
+        query = dropWhile (/= '?') url
+        encodedSecret = drop 1 . dropWhile (/= '=') $ query
         urlJson = BS.fromString encodedSecret
-        jsonStrict = urlDecode True
-        json = BL.fromStrict jsonStrict
-      case Aeson.decode json of
-        Just val -> do
-          putMVar (conf ^. newInvitation) val
-        Nothing -> do
-          putStrLn "gonimo: Warning: Received intentent that could not be handled: '" <> url <> "'"
+        json = urlDecode True urlJson
+      case Aeson.decodeStrict json of
+        Just val
+          -> putMVar (conf ^. Gonimo.newInvitation) val
+        Nothing
+          -> putStrLn $ "gonimo: Warning: Received intentent that could not be handled: '" <> url <> "'"
 
-  runWithIntent handleIntent Gonimo.main
+  runWithIntent handleIntent $ Gonimo.main conf
 
-runWithIntent :: (String -> JSM) -> JSM () -> IO ()
+runWithIntent :: (String -> IO ()) -> JSM () -> IO ()
 runWithIntent handleIntent jsm = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
@@ -52,7 +54,7 @@ runWithIntent handleIntent jsm = do
         putStrLn $ "gonimo:  intent: " <> intent
         putStrLn $ "gonimo: intent data: " <> intentData
         startMainWidget a startPage jsm
-    , _activityCallbacks_onNewIntent = \intent intentData -> handleIntent intentData
+    , _activityCallbacks_onNewIntent = \_ intentData -> handleIntent intentData
     }
   forever $ threadDelay 1000000000
 
---
+++ front/src/Gonimo/Client/Model.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
+{-|
+Module      : Gonimo.Client.Account
+Description : Types, classes and utilities for dealing with models and model configs.
+Copyright   : (c) Robert Klotzner, 2018
+-}
+module Gonimo.Client.Model ( -- * Types & Constraint types
+                             IsConfig
+                           ) where
+
+import Data.Default
+import Data.Semigroup
+import Reflex.Class.Extended
+
+-- | Types that have the properties that we demand from configurations.
+--
+--   Types must be a Monoid, so we can combine configs and have a default
+--   config. They should also have a Default instance, with def being mempty.
+--   Also they need to be Flattenable so we can return them from dynamic views.
+type IsConfig c t = (Default (c t), Monoid (c t), Flattenable c, Semigroup (c t))
