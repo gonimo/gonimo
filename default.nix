be0e4c4f96244bd19ca3f893854a734e828d457f
Robert Klotzner <robert.klotzner@gmx.at>
2018-04-09 10:46:51 +0200

    Version bump

--- default.nix
+++ default.nix
@@ -8,8 +8,8 @@ let
                    then "app.gonimo.com"
                    else "app.alpha.gonimo.com";
 
-  androidVersionCode = "";
-  androidVersionName = "";
+  androidVersionCode = "6";
+  androidVersionName = "1.0.1.2";
 
   androidIsRelease = builtins.pathExists ./release-key.nix;
 in
