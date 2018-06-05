bc857a5cf611f8375bdf8bf8766b4f420063c3ea
Robert Klotzner <robert.klotzner@gmx.at>
2018-06-05 15:11:13 +0200

    Made localstorage usage more convenient.

--- front/src/Gonimo/Client/Storage.hs
+++ front/src/Gonimo/Client/Storage.hs
@@ -1,15 +1,32 @@
-module Gonimo.Client.Storage (getItem
+module Gonimo.Client.Storage ( getItemLocal
+                             , setItemLocal
+                             , getItem
                              , setItem
                              , GHCJS.Storage) where
 
-import qualified GHCJS.DOM.Storage as GHCJS
-import Control.Monad.IO.Class (MonadIO)
-import           GHCJS.DOM.Types (fromJSString, JSString, toJSString, MonadJSM)
-import qualified Data.Aeson as Aeson
-import Data.Aeson (FromJSON, ToJSON)
-import qualified Data.Text.Encoding as T
-import qualified Data.ByteString.Lazy as BL
+import           Control.Monad.IO.Class (MonadIO)
+import           Data.Aeson             (FromJSON, ToJSON)
+import qualified Data.Aeson             as Aeson
+import qualified Data.ByteString.Lazy   as BL
+import qualified Data.Text.Encoding     as T
+import qualified GHCJS.DOM              as DOM
+import qualified GHCJS.DOM.Storage      as GHCJS
+import           GHCJS.DOM.Types        (JSString, MonadJSM, fromJSString,
+                                         toJSString)
+import qualified GHCJS.DOM.Window       as Window
 
+
+-- | Retrieve data from local storage.
+getItemLocal ::  (MonadIO m, ToJSON (key result), FromJSON result, MonadJSM m) => key result -> m (Maybe result)
+getItemLocal key = do
+    storage <- Window.getLocalStorage =<< DOM.currentWindowUnchecked
+    getItem storage key
+
+-- | Write data to local storage.
+setItemLocal :: (MonadIO m, ToJSON (key data'), ToJSON data', MonadJSM m) => key data' -> data' -> m ()
+setItemLocal key val = do
+    storage <- Window.getLocalStorage =<< DOM.currentWindowUnchecked
+    setItem storage key val
 
 getItem ::  (MonadIO m, ToJSON (key result), FromJSON result, MonadJSM m) => GHCJS.Storage -> key result -> m (Maybe result)
 getItem storage key = (fromJsonString =<<) <$> GHCJS.getItem storage (toJsonString key)
